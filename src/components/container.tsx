b4f6ffb8ddbb6b08c893e0ebfd07eb00e90914b0
DevDugg <mr.aydogdy02@gmail.com>
2025-08-31 00:34:00 +0500

    added container

---
+++ src/components/container.tsx
@@ -0,0 +1,22 @@
+import { cn } from "@/utils/cn";
+import { PropsWithChildren } from "react";
+
+interface IContainer extends PropsWithChildren {
+  className?: string;
+  parentClassName?: string;
+}
+
+const Container = ({ children, className, parentClassName }: IContainer) => {
+  return (
+    <div
+      className={cn(
+        "container mx-auto px-20 max-w-[680px] w-full border-x border-dashed",
+        parentClassName,
+      )}
+    >
+      {className ? <div className={cn("container-inner", className)}>{children}</div> : children}
+    </div>
+  );
+};
+
+export default Container;
